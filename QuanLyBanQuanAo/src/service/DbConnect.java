329213b334c540de8853e7fb99da6d3c5853efda
khoa2k4a <khoanvph37917@fpt.edu.vn>
2023-11-03 18:45:15 +0700

    khoa

--- QuanLyBanQuanAo/src/service/DbConnect.java
+++ QuanLyBanQuanAo/src/service/DbConnect.java
@@ -13,7 +13,7 @@ public class DbConnect {
     private static final String PASSWORD = "khoa710a";
     private static final String SERVER = "localhost";
     private static final String PORT = "1433";
-    private static final String DATABASE_NAME = "EduSys";
+    private static final String DATABASE_NAME = "POLYCLOTHES";
     private static final boolean USING_SSL = false;
    
     private static String CONNECT_STRING;
