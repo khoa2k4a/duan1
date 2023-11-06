916628d2afa26230d29e8bb59a242fcb4de1502b
khoa2k4a <khoanvph37917@fpt.edu.vn>
2023-11-06 17:44:33 +0700

    khoa

---
+++ QuanLyBanQuanAo/src/model/TenSP.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class TenSP {
+    private int id;
+    private String tenSP;
+
+    public TenSP() {
+    }
+
+    public TenSP(int id, String tenSP) {
+        this.id = id;
+        this.tenSP = tenSP;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTenSP() {
+        return tenSP;
+    }
+
+    public void setTenSP(String tenSP) {
+        this.tenSP = tenSP;
+    }
+    
+}
