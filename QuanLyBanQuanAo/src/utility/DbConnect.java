9632417184bf36eda625f125ddbb85b2ff4ef97f
khoa2k4a <khoanvph37917@fpt.edu.vn>
2023-11-08 22:40:37 +0700

    khoa

--- QuanLyBanQuanAo/src/utility/DbConnect.java
+++ QuanLyBanQuanAo/src/utility/DbConnect.java
@@ -2,86 +2,58 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package utility;
+package service;
 
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-
-/**
- *
- * @author admin
- */
 public class DbConnect {
-
-    public static final String HOSTNAME = "localhost";
-    public static final String PORT = "1433";
-    public static final String DBNAME = "POLYCLOTHES";
-    public static final String USERNAME = "sa";
-    public static final String PASSWORD = "khoa710a";
-
-    /**
-     * Get connection to MSSQL Server
-     *
-     * @return Connection
-     */
-    public static Connection getConnection() {
-
-// Create a variable for the connection string.
-        String connectionUrl = "jdbc:sqlserver://" + HOSTNAME + ":" + PORT + ";"
-                + "databaseName=" + DBNAME + ";encrypt=true;trustservercertificate=true";
-
+    private static final String USERNAME = "sa";
+    private static final String PASSWORD = "khoa710a";
+    private static final String SERVER = "localhost";
+    private static final String PORT = "1433";
+    private static final String DATABASE_NAME = "POLYCLOTHES";
+    private static final boolean USING_SSL = false;
+   
+    private static String CONNECT_STRING;
+   
+    static {
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            return DriverManager.getConnection(connectionUrl, USERNAME, PASSWORD);
-        } // Handle any errors that may have occurred.
-        catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace(System.out);
+           
+            StringBuilder connectStringBuilder = new StringBuilder();
+            connectStringBuilder.append("jdbc:sqlserver://")
+                    .append(SERVER).append(":").append(PORT).append(";")
+                    .append("databaseName=").append(DATABASE_NAME).append(";")
+                    .append("user=").append(USERNAME).append(";")
+                    .append("password=").append(PASSWORD).append(";")
+                    ;
+            if (USING_SSL) {
+                connectStringBuilder.append("encrypt=true;trustServerCertificate=true;");
+            }
+            CONNECT_STRING = connectStringBuilder.toString();
+            System.out.println("Connect String có dạng: " + CONNECT_STRING);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DbConnect.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return null;
     }
-//    public static PreparedStatement getStmt(String sql, Object[] args) throws SQLException{
-//        Connection con = getConnection();
-//        PreparedStatement stmt;
-//        if(sql.trim().startsWith("{")){
-//            stmt = con.prepareCall(sql);
-//        }else{
-//            stmt = con.prepareStatement(sql);
-//        }
-//        for(int i=0; i < args.length; i++){
-//            stmt.setObject(i+1, args[i]);
-//        }
-//        return stmt;
-//    }
-//    public static ResultSet query(String sql, Object[] args) throws SQLException{
-//        PreparedStatement stmt = getStmt(sql, args);
-//        return stmt.executeQuery();
-//    }
-//    public static Object value(String sql, Object[] args){
-//        try {
-//            ResultSet rs = query(sql, args);
-//            if(rs.next()){
-//                return rs.getObject(0);
-//            }
-//            rs.getStatement().getConnection().close();
-//            return null;
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-//    public static int update(String sql, Object[] args){
-//        try {
-//            PreparedStatement stmt = getStmt(sql, args);
-//            try {
-//                return stmt.executeUpdate();
-//            } finally {
-//                stmt.getConnection().close();
-//            }
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-
-    public static void main(String[] args) {
-        System.out.println(getConnection());
+   
+    public static Connection getConnection()  {
+         try {
+             return DriverManager.getConnection(CONNECT_STRING);
+         } catch (SQLException ex) {
+             ex.printStackTrace();
+             Logger.getLogger(DbConnect.class.getName()).log(Level.SEVERE, null, ex);
+             return null;
+         }
+    }
+   
+    public static void main(String[] args) throws Exception {
+        Connection conn = getConnection();
+        DatabaseMetaData dbmt = conn.getMetaData();
+        System.out.println(dbmt.getDriverName());
+        System.out.println(dbmt.getDatabaseProductName());
+        System.out.println(dbmt.getDatabaseProductVersion());
     }
 }
