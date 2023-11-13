0b0022b69b1ff050abc31d05853dd5b8a52752ea
khoa2k4a <khoanvph37917@fpt.edu.vn>
2023-11-13 20:43:01 +0700

    khoa

---
+++ QuanLyBanQuanAo/src/Application.java
@@ -0,0 +1,20 @@
+
+import java.awt.Frame;
+import view.LoginJDialog;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author admin
+ */
+public class Application {
+
+    private static Frame parent;
+
+    public static void main(String[] args) {
+        new LoginJDialog(parent, true).setVisible(true);
+    }
+}
